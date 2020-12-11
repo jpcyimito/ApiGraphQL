16e4edd6742efe296602d2bf7c8aa82962eb15d6
jpcyimito <yimitopc@gmail.com>
2020-12-11 00:11:53 -0500

    configuration

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,19 @@
 import express from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
 
 class Server {
     public app: express.Application;
 
     constructor() {
         this.app = express();
+        this.config();
     }
 
     config() {
-
+        this.app.set('port', process.env.PORT || 3000)
+        this.app.use(morgan('dev'));
+        this.app.use(helmet());
     }
 
     routes() {
@@ -16,8 +21,11 @@ class Server {
     }
 
     start() {
-
+        this.app.listen(this.app.get('port'), () => {
+            console.log('Server on port', this.app.get('port'))
+        });
     }
 }
 
-new Server()
+const server = new Server()
+server.start();
